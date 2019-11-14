f3b91104033869658561fa2f28624dcfa2452da6
Andreas <andreas.astr@gmail.com>
2019-11-14 10:27:07 +0100

    init commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <VideoPlayer msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import VideoPlayer from "./components/VideoPlayer.vue";
+
+@Component({
+  components: {
+    VideoPlayer
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <VideoPlayer msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import VideoPlayer from "@/components/VideoPlayer.vue";
+
+export default {
+  name: "home",
+  components: {
+    VideoPlayer
+  }
+};
+</script>
