816bec604e2905eb15eee2d8963fbee1ba986754
Andreas <andreas.astr@gmail.com>
2019-11-14 11:53:17 +0100

    (added): VideoComponent

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -3,14 +3,15 @@
 </template>
 
 <script lang="ts">
+/// <reference types="@types/dom-mediacapture-record" />
 import { Component, Prop, Vue } from "vue-property-decorator";
 
 @Component
 export default class VideoPlayer extends Vue {
   @Prop() private msg!: string;
+  mediaRecorder: MediaRecorder = null;
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
